d077bb89a9b6c9b30b3a4131553492bac0a8cebe
Dave Penfold <davepenfold@gmail.com>
2019-06-07 19:02:19 -0700

    Update home page content

--- src/index.js
+++ src/index.js
@@ -9,6 +9,16 @@ const App = () => (
       <div className="row">
         <div className="column"><a href="/">velox.cc</a> / road cycling and technology</div>
       </div>
+      <main>
+        <h2 style={{ marginTop: '4rem' }}>Projects</h2>
+
+        <ul>
+          <li><a href="/awesome-road-bike">Awesome Road Bike!</a> - A curated list of road bike related stuff!</li>
+        </ul>
+        <p>... more to come!</p>
+
+        <p>If you have any feedback or questions please <a href="javascript:void" onClick={() => window.location = 'mailto:hello@velox.cc' }>contact us</a>.</p>
+      </main>
     </header>
   </div>
 );
