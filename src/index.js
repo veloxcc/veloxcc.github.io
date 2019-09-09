465a49fff66cccc1b05baf26ae75a06a6bdaed4e
Dave Penfold <davepenfold@gmail.com>
2019-09-08 23:11:31 -0700

    Add link to news project

--- src/index.js
+++ src/index.js
@@ -13,8 +13,9 @@ const App = () => (
         <h2 style={{ marginTop: '4rem' }}>Projects</h2>
 
         <ul>
-          <li><a href="/awesome-road-bike">Awesome Road Bike!</a> - A curated list of road bike related stuff!</li>
-          <li><a href="/gear-ratios">Gear Ratios!</a> - A bike calculator that displays gear ratios and speed.</li>
+          <li><a href="/news">Cycling News</a> - todays most popular cycling news, reviews and more.</li>
+          <li><a href="/awesome-road-bike">Awesome Road Bike!</a> - a curated list of road bike related stuff!</li>
+          <li><a href="/gear-ratios">Gear Ratios!</a> - a bike calculator that displays gear ratios and speed.</li>
         </ul>
 
         <p>If you have any feedback or questions please <a href="mailto:hello@velox.cc">contact us</a>.</p>
@@ -23,5 +24,5 @@ const App = () => (
   </div>
 );
 
-var mountNode = document.getElementById("app");
+var mountNode = document.getElementById('app');
 ReactDOM.render(<App />, mountNode);
